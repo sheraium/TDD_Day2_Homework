fa99f2577b664ec9b1a530aa6113b743c3170e06
Yong xiu <sheraium@gmail.com>
2017-08-28 11:50:23 +0800

    綠燈 Add Test IBuyAndNeedPayTest_一二三四集各買一本_價格320元

--- TddDay2/PotterShoppingCart.cs
+++ TddDay2/PotterShoppingCart.cs
@@ -14,14 +14,19 @@ namespace TddDay2
         {
             double result = 0;
             double discountRate = 1;
-            
+
             switch (this.bookAmount)
             {
                 case 2:
                     discountRate = 0.95;
                     break;
+
                 case 3:
                     discountRate = 0.9;
+                    break;
+
+                case 4:
+                    discountRate = 0.8;
                     break;
             }
 
