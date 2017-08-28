2314ee944162fd4d6806c044c466658c95d3f22e
Yong xiu <sheraium@gmail.com>
2017-08-28 10:40:29 +0800

    紅燈 AddTest IBuyAndNeedPayTest_第一集買一本_價格100元

---
+++ TddDay2/PotterShoppingCartTests.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TddDay2
+{
+    [TestClass]
+    public class PotterShoppingCartTests
+    {
+        [TestMethod]
+        public void IBuyAndNeedPayTest_第一集買一本_價格100元()
+        {
+            var sut = new PotterShoppingCart();
+
+            var actual = sut.IBuyAndNeedPay("Harry Potter 1", 1);
+
+            var expected = 100;
+            Assert.AreEqual(expected, actual);
+        }
+    }
+
+    public class PotterShoppingCart
+    {
+        public int IBuyAndNeedPay(string book, int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
