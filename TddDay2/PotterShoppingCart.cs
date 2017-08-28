853aa4aadc40f08c4a59179fcaf77e271cc102f9
Yong xiu <sheraium@gmail.com>
2017-08-28 11:39:31 +0800

    綠燈 AddTest IBuyAndNeedPayTest_一二集各買一本_價格190元

--- TddDay2/PotterShoppingCart.cs
+++ TddDay2/PotterShoppingCart.cs
@@ -2,14 +2,23 @@ namespace TddDay2
 {
     public class PotterShoppingCart
     {
+        private int bookAmount = 0;
+
         public PotterShoppingCart IBuy(string book, int amount)
         {
+            this.bookAmount += amount;
             return this;
         }
 
         public int NeedPay()
         {
-            return 100;
+            double result = 0;
+            double discountRate = 1;
+
+            if (this.bookAmount == 2) discountRate = 0.95;
+
+            result = 100 * this.bookAmount * discountRate;
+            return (int)result;
         }
     }
 }
