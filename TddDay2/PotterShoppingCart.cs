c6ac716756742839a8810f381d82e02bee82fa9f
Yong xiu <sheraium@gmail.com>
2017-08-28 13:41:13 +0800

    重構 PotterShoppingCart.NeedPay()

--- TddDay2/PotterShoppingCart.cs
+++ TddDay2/PotterShoppingCart.cs
@@ -6,12 +6,10 @@ namespace TddDay2
 
     public class PotterShoppingCart
     {
-        private int bookAmount = 0;
-        readonly Dictionary<string, int> orderedBooks = new Dictionary<string, int>();
+        private readonly Dictionary<string, int> orderedBooks = new Dictionary<string, int>();
 
         public PotterShoppingCart IBuy(string book, int amount)
         {
-            this.bookAmount += amount;
             this.orderedBooks.Add(book, amount);
             return this;
         }
@@ -19,47 +17,58 @@ namespace TddDay2
         public int NeedPay()
         {
             double result = 0;
-            double discountRate = 1;
+            int[] amountArray = GetOrderedBooksAmountArray();
 
-            var books = new int[this.orderedBooks.Count];
-            Array.Copy(this.orderedBooks.Values.ToArray(), books, this.orderedBooks.Count);
-            
-            while (books.Any(amount => amount > 0))
+            while (amountArray.Any(amount => amount > 0))
             {
                 int booksAmount = 0;
-                for (int i = 0; i < books.Length; i++)
+                for (int i = 0; i < amountArray.Length; i++)
                 {
-                    if (books[i] > 0)
+                    if (amountArray[i] > 0)
                     {
                         booksAmount++;
-                        books[i]--;
+                        amountArray[i]--;
                     }
                 }
 
-                switch (booksAmount)
-                {
-                    case 2:
-                        discountRate = 0.95;
-                        break;
-
-                    case 3:
-                        discountRate = 0.9;
-                        break;
-
-                    case 4:
-                        discountRate = 0.8;
-                        break;
-
-                    case 5:
-                        discountRate = 0.75;
-                        break;
-                    default:
-                        discountRate = 1d;
-                        break;
-                }
-                result += 100 * booksAmount * discountRate;
+                result += 100 * booksAmount * GetDiscountRate(booksAmount);
             }
             return (int)result;
+        }
+
+        private int[] GetOrderedBooksAmountArray()
+        {
+            var booksAmountArray = new int[this.orderedBooks.Count];
+            Array.Copy(this.orderedBooks.Values.ToArray(), booksAmountArray, this.orderedBooks.Count);
+            return booksAmountArray;
+        }
+
+        private double GetDiscountRate(int booksAmount)
+        {
+            double discountRate;
+            switch (booksAmount)
+            {
+                case 2:
+                    discountRate = 0.95;
+                    break;
+
+                case 3:
+                    discountRate = 0.9;
+                    break;
+
+                case 4:
+                    discountRate = 0.8;
+                    break;
+
+                case 5:
+                    discountRate = 0.75;
+                    break;
+                default:
+                    discountRate = 1.0;
+                    break;
+            }
+
+            return discountRate;
         }
     }
 }
